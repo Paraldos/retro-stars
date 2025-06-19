f0b6c18c555686f58d988a70e7192f28f760dd56
Paraldos <hillbrecht.s@gmail.com>
2025-06-19 10:19:21 +0200

    refactored sound effects

---
+++ classes/sound_effect.gd
@@ -0,0 +1,15 @@
+class_name SoundEffect
+extends Node2D
+
+@export var pitch_range = 0.05
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	rng.randomize()
+	for child in get_children():
+		child.bus = &"SFX"
+
+func _play():
+	var effect_number = rng.randi_range(0, get_child_count()-1)
+	get_children()[effect_number].pitch_scale = rng.randf_range(1 - pitch_range, 1 + pitch_range)
+	get_children()[effect_number].play()
--- strato_defense/explosion/explosion.gd
+++ strato_defense/explosion/explosion.gd
@@ -1,24 +1,14 @@
 extends CPUParticles2D
 
-@onready var sound_effects = [
-	%Sound1,
-	%Sound2
-]
-var rng = RandomNumberGenerator.new()
+@onready var sound_effect: SoundEffect = %SoundEffect
 
 func _ready() -> void:
-	rng.randomize()
 	_start_particles()
-	_start_sound()
+	sound_effect._play()
 
 func _start_particles():
 	restart()
 	one_shot = true
-
-func _start_sound():
-	var effect_number = rng.randi_range(0, sound_effects.size()-1)
-	sound_effects[effect_number].pitch_scale = rng.randf_range(0.95, 1.05)
-	sound_effects[effect_number].play()
 
 func _on_finished() -> void:
 	queue_free()
