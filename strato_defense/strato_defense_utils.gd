4893eb52e1af9ce3a18acb32f80a468ac91a2f07
Paraldos <hillbrecht.s@gmail.com>
2025-06-22 19:10:53 +0200

    added reset to strato defense

--- strato_defense/strato_defense_utils.gd
+++ strato_defense/strato_defense_utils.gd
@@ -4,6 +4,10 @@ var buildings = 0
 var main_gun_intact = true
 var explosion_template = preload('res://strato_defense/explosion/explosion.tscn')
 
+func _reset():
+	buildings = 0
+	main_gun_intact = true
+
 func _spawn_explosion(position):
 	var explosion = explosion_template.instantiate()
 	explosion.global_position = position
