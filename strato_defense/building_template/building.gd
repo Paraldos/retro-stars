fdaeaa9d43085a3172d550cfd4c1ac79482c31f5
Paraldos <hillbrecht.s@gmail.com>
2025-06-25 09:48:24 +0200

    added background to buildings and gun

--- strato_defense/building_template/building.gd
+++ strato_defense/building_template/building.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 @onready var torso_line: Line2D = %TorsoLine
 @onready var collision_polygon: CollisionPolygon2D = %CollisionPolygon
+@onready var background_polygon: Polygon2D = %BackgroundPolygon
 
 func _ready() -> void:
-	torso_line.position.y -= 7
+	position.y -= 7
+	background_polygon.polygon = collision_polygon.polygon
 	torso_line.points = collision_polygon.polygon
 	await get_tree().create_timer(0.1).timeout
 	StratoDefenseUtils.buildings += 1
